c3dc9f753e460728494172cf00921c016e36f105
pjexposito <pjexposito@gmail.com>
2015-10-23 21:53:38 +0200

    Nueva cosas

--- src/pebble-js-app.js
+++ src/pebble-js-app.js
@@ -30,11 +30,13 @@ function procesa_mensaje(mensaje)
 {
   var encender = -1; // Encender no tiene valor asignado
   var mensaje_separado = mensaje.split(" ");
-  console.log("Mensaje total: "+ mensaje + ". Separado es 1: "+ mensaje_separado[0]+" 2:"+mensaje_separado[1]+" 3:"+mensaje_separado[3]+", etc.");
-  var w_encender = ["enciende", "enchufa", "activa", "prende", "encender", "activar", "prender", "enchufar"];
-  var w_apagar = ["apaga", "desenchufa", "desactiva", "para", "apagar", "desenchufar", "desactivar", "parar"];
-
-
+  console.log("Mensaje total: "+ mensaje + ". Separado es 1: "+ mensaje_separado[0]+" 2:"+mensaje_separado[1]+" 3:"+mensaje_separado[2]+", etc.");
+  
+  // El formato es opciones es {Palabra_hablada: PIN_EN_ARDUINO}
+  var opciones = {"luz" : '0', "lámpara" : '1', "led": '2'};
+  
+  var w_encender = ["conecta", "enciende", "enchufa", "activa", "prende", "conectar","encender", "activar", "prender", "enchufar"];
+  var w_apagar = ["desconecta", "apaga", "desenchufa", "desactiva", "para", "apagar", "desconectar", "desenchufar", "desactivar", "parar"];
 
   if (w_encender.indexOf(mensaje_separado[0]) > -1)
     {
@@ -42,8 +44,9 @@ function procesa_mensaje(mensaje)
     }
   if (w_apagar.indexOf(mensaje_separado[0]) > -1)
     {
-        encender = true;
+        encender = false;
     }
+  console.log("El valor de encender es: "+encender+" para "+  opciones[mensaje_separado[2]]);
 }
 
 Pebble.addEventListener("appmessage",
@@ -51,9 +54,8 @@ Pebble.addEventListener("appmessage",
   console.log("Funcion para procesar datos");
   console.log(sitios.uno);
   var mensaje = e.payload.KEY_CADENA_ENVIADA;
-  procesa_mensaje(mensaje);
+  procesa_mensaje(mensaje.toLowerCase());
 	//ObtenDatos();
-    var dict = {"1" : 1};
 
     	Pebble.sendAppMessage(dict);
 
